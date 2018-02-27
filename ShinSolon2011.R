e457cd7321a04e6cab00ffbae7505004381603fe
Hasan Kadir Tosun <hktosun@gmail.com>
2018-02-26 20:43:52 -0600

    update

--- ShinSolon2011.R
+++ ShinSolon2011.R
@@ -3,28 +3,19 @@ rm(list = ls(all=TRUE))
 library("tidyverse")
 library("e1071")
 library("reshape2")
-library(xtable)
-library(stargazer)
+library("xtable")
 
 
-setwd("func")
-files.sources = list.files()
+files.sources = list.files("func", full.names = TRUE)
 sapply(files.sources, source)
-setwd("..")
 
 years <- c(seq(1971,1996,1), seq(1998,2006,2));
-
-
-
-
-
 
 if("data" %in% list.files()){
   T <- length(years)
   
   cpi0 <- c(60.9, 63.9, 66.7, 68.7, 73.0, 80.3, 86.9, 91.9,  97.7,  104.4, 114.4, 127.1, 139.2, 147.6, 153.9, 160.2, 165.7, 168.7, 174.4, 180.8, 188.6, 198.0, 205.1, 210.3, 215.5, 220.1, 231.4, 239.7, 252.9, 264.2, 277.4)
   cpi1 <- c(66.7, 68.7, 73.0, 80.3, 86.9, 91.9, 97.7, 104.4, 114.4, 127.1, 139.2, 147.6, 153.9, 160.2, 165.7, 168.7, 174.4, 180.8, 188.6, 198.0, 205.1, 210.3, 215.5, 220.1, 225.4, 231.4, 239.7, 252.9, 264.2, 277.4, 296.1)
-  
   
   age_vec <- c()
   wage_vec <- c()
@@ -48,10 +39,10 @@ if("data" %in% list.files()){
   	mat5 <- cbind(mat5, getNumbersForFigure5(data))
   }
 
-  rm(cpi0, cpi1, files, files.sources, y)
  
-
   summaryStatistics(age_vec, wage_vec)
+  
+  rm(cpi0, cpi1, files, files.sources, y, df_c, data, age_vec, wage_vec)
   
   write.table(cbind(years, t(mat2)), file="output/fig2data.txt", sep = ",", row.names=FALSE, col.names=c("year", "Wages & salaries", "Wages & salaries with ages 30-54", "Total Labor Income (Dynan et al.)", "Wages & salaries without controlling for age", "Total Labor Income excluding farm & business income"))
   write.table(cbind(years, t(mat3)), file="output/fig3data.txt", sep = ",", row.names=FALSE, col.names=c("year", "p90","p75","median","p25","p10"))
